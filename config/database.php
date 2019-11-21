68ead9ff1b5b5073c2cfdc587b6eda2de281f311
Eduardo Hernández <joredher30@gmail.com>
2019-11-21 01:27:33 -0500

    Nueva configuración en config.

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mesa_servicio'),
+            'username' => env('DB_USERNAME', 'capresoca'),
+            'password' => env('DB_PASSWORD', 'capremesa'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
